af2772f95b6c9a156515da12832b2229b4e22d1e
Lipien <adam@pawtel.pl>
2020-01-08 18:26:27 +0100

    Person

--- CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
+++ CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
@@ -2,6 +2,7 @@ package pl.pawtel.classes_constructors_inheritance;
 
 public class Main {
     public static void main(String[] args) {
+        // test for SimpleCalculator
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         simpleCalculator.setFirstNumber(5.0);
         simpleCalculator.setSecondNumber(4.0);
@@ -9,5 +10,19 @@ public class Main {
         System.out.println("Substract: " + simpleCalculator.getSubstractionResult());
         System.out.println("Multiply: " + simpleCalculator.getMultiplicationResult());
         System.out.println("Divide: " + simpleCalculator.getDivisionResult());
+
+        // test for Person
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
     }
 }
