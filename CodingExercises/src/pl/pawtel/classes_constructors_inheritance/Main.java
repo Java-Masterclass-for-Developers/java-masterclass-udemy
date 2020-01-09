d560a19940233c356bfaa743d30372ee332782b2
Lipien <adam@pawtel.pl>
2020-01-09 19:21:28 +0100

    exercise: Point

--- CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
+++ CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
@@ -25,13 +25,28 @@ public class Main {
         person.setLastName("Smith");    // lastName is set to Smith
         System.out.println("fullName= " + person.getFullName());
 
-       // test for Wall
-        Wall wall = new Wall(5,4);
+        // test for Wall
+        Wall wall = new Wall(5, 4);
         System.out.println("area= " + wall.getArea());
 
         wall.setHeight(-1.5);
         System.out.println("width= " + wall.getWidth());
         System.out.println("height= " + wall.getHeight());
         System.out.println("area= " + wall.getArea());
+
+        // test for Point
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+
+        //OUTPUT Point
+        /*distance(0,0)= 7.810249675906654
+        distance(second)= 5.0
+        distance(2,2)= 5.0
+        distance()= 0.0*/
     }
 }
