b4aaee9eedf16e206b3e08586856134be3d5a25b
Lipien <adam@pawtel.pl>
2020-01-10 16:22:05 +0100

    Exercise: Cylinder

---
+++ CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Cylinder.java
@@ -0,0 +1,23 @@
+package pl.pawtel.classes_constructors_inheritance;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else
+            this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+
+        return Math.pow(getRadius(), 2) * Math.PI * height;
+    }
+}
--- CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
+++ CodingExercises/src/pl/pawtel/classes_constructors_inheritance/Main.java
@@ -48,5 +48,16 @@ public class Main {
         distance(second)= 5.0
         distance(2,2)= 5.0
         distance()= 0.0*/
+
+        // Circle TEST CODE:
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
+
     }
 }
