8fc7d5c88ccaee8c7d86f08090f0708836c20e48
Lipien <adam@pawtel.pl>
2020-01-14 10:47:01 +0100

    Encapsulation: enhanced player

--- CompositionEncapsulationPolymorphism/src/pl/pawtel/encapsulation/Main.java
+++ CompositionEncapsulationPolymorphism/src/pl/pawtel/encapsulation/Main.java
@@ -4,18 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("remaining health = " + player.healthRemaining());
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("remaining health = " + player.healthRemaining());
-
-        damage = 11;
-        player.health = 200;
-        player.loseHealth(damage);
-        System.out.println("remaining health = " + player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 200, "Sword");
+        System.out.println("Intial health is " + player.getHitPoints());
     }
 }
