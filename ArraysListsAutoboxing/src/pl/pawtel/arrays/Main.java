0fe6b42e6de185eed5c47b5872cd6290553ce4d5
Lipien <adam@pawtel.pl>
2020-01-14 13:11:03 +0100

    Array of Scanner input

--- ArraysListsAutoboxing/src/pl/pawtel/arrays/Main.java
+++ ArraysListsAutoboxing/src/pl/pawtel/arrays/Main.java
@@ -1,26 +1,36 @@
 package pl.pawtel.arrays;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        int[] myIntArray = new int[10];
-//        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        myIntArray[5] = 50;
-//        System.out.println(myIntArray[5]);
-//        System.out.println(myIntArray[6]);
-
-        for (int i = 0; i < myIntArray.length; i++) {
-            myIntArray[i] = i;
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
-        printArray(myIntArray);
-
-
+        System.out.println("The average is " + getAverage(myIntegers));
     }
 
-    public static void printArray(int[] array){
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("element " + i + ", value is " + array[i]);
+    public static int[] getIntegers(int number) {
+
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
         }
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
     }
 }
