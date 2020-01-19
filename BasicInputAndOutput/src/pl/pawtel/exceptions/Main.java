0c5d691bc5f90729dd8b23595c84a71e0c23401a
Lipien <adam@pawtel.pl>
2020-01-19 15:46:23 +0100

    Validation of scanner input

--- BasicInputAndOutput/src/pl/pawtel/exceptions/Main.java
+++ BasicInputAndOutput/src/pl/pawtel/exceptions/Main.java
@@ -1,13 +1,40 @@
 package pl.pawtel.exceptions;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int x = 98;
-        int y = 0;
-        System.out.println(divideLBYL(x, y));
-        System.out.println(divideEAFPP(x, y));
-        System.out.println(divide(x, y));
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(divideLBYL(x, y));
+//        System.out.println(divideEAFPP(x, y));
+//        System.out.println(divide(x, y));
+        int x = getIntLBYL();
+        System.out.println("x is  " + x);
+
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL() {
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Plaease enter an integer");
+        String input = s.next();
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            return Integer.parseInt(input);
+        }
+        return 0;
     }
 
     private static int divideLBYL(int x, int y) {
