7e9c45ac927b15eb7077e2eb0a62930123ee87d6
Lipien <adam@pawtel.pl>
2020-01-22 15:25:44 +0100

    simple lambda; Runnable

--- Lambdas/src/pl/pawtel/Main.java
+++ Lambdas/src/pl/pawtel/Main.java
@@ -2,6 +2,13 @@ package pl.pawtel;
 
 public class Main {
     public static void main(String[] args) {
+       new Thread(()-> System.out.println("Printing from the Runnable")).start();
+    }
+}
 
+class CodeToRun implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Printing from Runnable");
     }
 }
