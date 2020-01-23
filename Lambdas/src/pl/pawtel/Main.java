42ee430d54f252ccffbe84e06e0ed3ae6917f90a
Lipien <adam@pawtel.pl>
2020-01-23 15:57:08 +0100

    Comparator used as lambda expression

--- Lambdas/src/pl/pawtel/Main.java
+++ Lambdas/src/pl/pawtel/Main.java
@@ -2,21 +2,20 @@ package pl.pawtel;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       new Thread(()-> {
-           System.out.println("Printing from the Runnable");
-           System.out.println("Line 2");
-           System.out.println("This is line 3");
-       }).start();
+//        new Thread(() -> {
+//            System.out.println("Printing from the Runnable");
+//            System.out.println("Line 2");
+//            System.out.println("This is line 3");
+//        }).start();
 
-       Employee john = new Employee("John Doe", 30);
-       Employee tim = new Employee("Tim Buchalka", 21);
-       Employee jack = new Employee("Jack Lisowski", 44);
-       Employee snow = new Employee("Snow White", 22);
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Lisowski", 44);
+        Employee snow = new Employee("Snow White", 22);
 
         List<Employee> employees = new ArrayList<>();
         employees.add((john));
@@ -24,23 +23,24 @@ public class Main {
         employees.add((jack));
         employees.add((snow));
 
-        Collections.sort(employees, new Comparator<Employee>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                return employee1.getName().compareTo(employee2.getName());
+//        Collections.sort(employees, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//
+//            }
+//        });
 
-            }
-        });
+        Collections.sort(employees, (employee1, employee2) ->
+                employee1.getName().compareTo(employee2.getName()));
 
-        for (Employee employee : employees){
+        for (Employee employee : employees) {
             System.out.println(employee.getName());
         }
-
-
     }
 }
 
-class Employee{
+class Employee {
     private String name;
     private int age;
 
