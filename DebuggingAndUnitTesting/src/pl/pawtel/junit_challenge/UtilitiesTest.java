e22363cc8100588a3570aba1234a70c74bdbd78f
Lipien <adam@pawtel.pl>
2020-04-02 16:30:05 +0200

    challenge nr one

---
+++ DebuggingAndUnitTesting/src/pl/pawtel/junit_challenge/UtilitiesTest.java
@@ -0,0 +1,44 @@
+package pl.pawtel.junit_challenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @Test
+    public void everyNthChar() {
+        //Given
+        fail("Methods fails; write testing code");
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void removePairs() {
+        //Given
+        fail("Methods fails; write testing code");
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void converter() {
+        //Given
+        fail("Methods fails; write testing code");
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void nullIfOddLength() {
+        //Given
+        fail("Methods fails; write testing code");
+        //When
+
+        //Then
+    }
+}
