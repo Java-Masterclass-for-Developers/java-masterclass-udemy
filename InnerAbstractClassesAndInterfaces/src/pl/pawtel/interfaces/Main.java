2c1cf7f44c0f9ec368560ac94f343619d40222f8
Lipien <adam@pawtel.pl>
2020-06-16 15:30:34 +0200

    Methods implementation

--- InnerAbstractClassesAndInterfaces/src/pl/pawtel/interfaces/Main.java
+++ InnerAbstractClassesAndInterfaces/src/pl/pawtel/interfaces/Main.java
@@ -1,8 +1,11 @@
 package pl.pawtel.interfaces;
 
 public class Main {
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        ITelephone adamsPhone;
+        adamsPhone = new DeskPhone(1234456);
+        adamsPhone.powerOn();
+        adamsPhone.callPhone(1233456);
+        adamsPhone.answer();
     }
 }
